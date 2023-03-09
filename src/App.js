09ae0878eac49df0ef40b8b828428cca96895d8c
LiaPetrova <96750474+LiaPetrova@users.noreply.github.com>
2023-03-09 18:31:18 +0100

    Initialize App and Connect with Firestore

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import handleSubmit from './handles/handlesubmit';
+import { useRef } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const dataRef = useRef()
+
+    const submithandler = (e) => {
+        e.preventDefault()
+        handleSubmit(dataRef.current.value);
+        dataRef.current.value = ""
+    }
+    return (
+        <div className="App">
+            <form onSubmit={submithandler}>
+                <input type="text" ref={dataRef} />
+                <button type="submit">Save</button>
+            </form>
+        </div>
+    );
 }
 
 export default App;
