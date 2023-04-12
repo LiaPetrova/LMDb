5789abfd37563b46cc0c9a9f02933a9cf5b3b7ad
LiaPetrova <96750474+LiaPetrova@users.noreply.github.com>
2023-04-12 14:17:46 +0200

    fix text shortener with different window widths

--- src/utils/parsers.js
+++ src/utils/parsers.js
@@ -1,37 +1,65 @@
 
 export const useParsers = (() => {
     const durationParser = (time) => {
-                const minute = 1;
-                const hour = 60 * minute;
-            
-                if(time / hour >= 1) {
-                    return `${Math.floor(time/hour)}h ${time%hour}m`;
-                } else {
-                    return `${time%hour}m`;
-                }
-            };
-            
-            const textShortener = (text) => {
-                if(text.length > 367) {
-                    return `${text.slice(0, 366)}...`
-                } else {
-                    return text;
-                }
-            };
-            
-            const yearParser = (number) => {
-                if(number.toString().length === 4) {
-                    return number;
-                } else {
-                    return `(${number.toString().slice(0, 4)} - ${number.toString().slice(4)})`;
-                }
-            };
-        
-            return {
-                durationParser,
-                textShortener,
-                yearParser
+        const minute = 1;
+        const hour = 60 * minute;
+
+        if (time / hour >= 1) {
+            return `${Math.floor(time / hour)}h ${time % hour}m`;
+        } else {
+            return `${time % hour}m`;
+        }
+    };
+
+    const textShortener = (text) => {
+        let x = window.innerWidth || document.documentElement.clientWidth;
+        if (x <= 434) {
+            console.log(1);
+
+            if (text.length > 122) {
+                return `${text.slice(0, 121)}...`
+            } else {
+                return text;
             }
+            
+            
+        } else if (x <= 852) {
+            console.log(2);
+
+            if (text.length > 170) {
+                return `${text.slice(0, 168)}...`
+            } else {
+                return text;
+            }
+        } else if (x <= 1024) {
+            console.log(3);
+            if (text.length > 250) {
+                return `${text.slice(0, 249)}...`
+            } else {
+                return text;
+            }
+        } else {
+            if (text.length > 367) {
+                return `${text.slice(0, 366)}...`
+            } else {
+                return text;
+            }
+        }
+    };
+
+    const yearParser = (number) => {
+        if (number.toString().length === 4) {
+            return number;
+        } else {
+            return `(${number.toString().slice(0, 4)} - ${number.toString().slice(4)})`;
+        }
+    };
+
+    return {
+        durationParser,
+        textShortener,
+        yearParser
+    }
 });
 
 
