22c38446ba3edd5d2ef6d47f4f41ee5257582561
LiaPetrova <96750474+LiaPetrova@users.noreply.github.com>
2023-04-13 23:24:17 +0200

    update

--- src/components/layout/Header/ScrollButton/ScrollButton.js
+++ src/components/layout/Header/ScrollButton/ScrollButton.js
@@ -3,33 +3,35 @@ import styles from './ScrollButton.module.css';
 
 export const ScrollButton = () => {
 
-    const [visible,setVisible] = useState(false);
+    const [visible, setVisible] = useState(false);
     useEffect(() => {
-        
-  window.addEventListener('scroll', toggleVisible);
+
+        window.addEventListener('scroll', toggleVisible);
     }, []);
 
     const toggleVisible = () => {
         const scrolled = document.documentElement.scrollTop;
-        if (scrolled > 300){
-          setVisible(true)
-        } 
-        else if (scrolled <= 300){
-          setVisible(false)
+        if (scrolled > 350) {
+            setVisible(true)
         }
-      };
+        else if (scrolled <= 350) {
+            setVisible(false)
+        }
+    };
 
-    const scrollToTop = () =>{
+    const scrollToTop = () => {
         window.scrollTo({
-          top: 0, 
-          behavior: 'smooth'
-          /* you can also use 'auto' behaviour
-             in place of 'smooth' */
+            top: 0,
+            behavior: 'smooth'
+            /* you can also use 'auto' behaviour
+               in place of 'smooth' */
         });
-      };
+    };
     return (
-        <button onClick={scrollToTop} className={`btn ${styles['scroll-to-top']} ${visible ? styles.visible : styles.hidden}`}>
-            <i className="fa-solid fa-arrow-up"></i>
-        </button>
+        <div className={styles['scroll-to-top-wrapper']}>
+            <button onClick={scrollToTop} className={`btn ${styles['scroll-to-top']} ${visible ? styles.visible : styles.hidden}`}>
+                <i className="fa-solid fa-arrow-up"></i>
+            </button>
+        </div>
     );
 };
